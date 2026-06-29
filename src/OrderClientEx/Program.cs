378b29b975f0ded6f63ef75709d11bba0eab67f2
nwcollasz <nicollas.b@aluno.ifsp.edu.br>
2026-06-28 21:27:49 -0300

    rename

--- src/OrderClientEx/Program.cs
+++ src/OrderClientEx/Program.cs
@@ -9,34 +9,34 @@ namespace OrderClientEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter client data: ");
-            Console.Write("Name: ");
+            Console.WriteLine("Dados do cliente: ");
+            Console.Write("Nome: ");
             string clientName = Console.ReadLine();
             Console.Write("Email: ");
             string email = Console.ReadLine();
-            Console.Write("Birth date (DD/MM/YYYY): ");
+            Console.Write("Data de nascimento: ");
             DateTime birthDate = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine("Enter order data: ");
+            Console.WriteLine("Dados do pedido: ");
             Console.Write("Status: ");
             OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
 
             Client client = new Client(clientName, email, birthDate);
             Order order = new Order(DateTime.Now, status, client);
 
-            Console.Write("How many items to this order? ");
+            Console.Write("Quantos items para esse pedido? ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 1; i <= n; i++)
             {
-                Console.WriteLine($"Enter #{i} item data:");
-                Console.Write("Product name: ");
+                Console.WriteLine($"Digite os dados do item #{i}:");
+                Console.Write("Produto: ");
                 string productName = Console.ReadLine();
-                Console.Write("Product price: ");
+                Console.Write("Preço: ");
                 double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
                 Product product = new Product(productName, price);
 
-                Console.Write("Quantity: ");
+                Console.Write("Quantidade: ");
                 int quantity = int.Parse(Console.ReadLine());
 
                 OrderItem orderItem = new OrderItem(quantity, price, product);
@@ -45,7 +45,7 @@ namespace OrderClientEx
             }
 
             Console.WriteLine();
-            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine("PEDIDOS:");
             Console.WriteLine(order);
            
         }
