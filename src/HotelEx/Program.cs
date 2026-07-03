000bdff0698d071fd4a3a13672d15c3bd812b2ba
nwcollasz <nicollas.b@aluno.ifsp.edu.br>
2026-07-03 15:08:55 -0300

    exemplo de try catch

---
+++ src/HotelEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HotelEx.Entities.Exceptions;
+using HotelEx.Entities;
+
+namespace HotelEx.Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Número do quarto: ");
+                int numeroQuarto = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Data de entrada (dd/mm/yyyy): ");
+                DateTime dataEntrada = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Data de saída (dd/mm/yyyy): ");
+                DateTime dataSaida = DateTime.Parse(Console.ReadLine());
+
+                Reserva reserva = new Reserva(numeroQuarto, dataEntrada, dataSaida);
+                Console.WriteLine("Reserva: " + reserva);
+            }
+            catch (DomainException ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+        }
+
+    }
+}
