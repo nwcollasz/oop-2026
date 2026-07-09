fc7a1b50972c1a931170980b220f1e08f4d76fda
nwcollasz <nicollas.b@aluno.ifsp.edu.br>
2026-07-08 21:53:08 -0300

    restrições de generics

---
+++ exercicios/generics/restricao-ex/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace restricao_ex.Entities {
+    class Product : IComparable {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        public int CompareTo(object obj) {
+            if (!(obj is Product)) {
+                throw new ArgumentException("Erro: o argument não é um produto");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+
+        public override string ToString() {
+            return Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ exercicios/generics/restricao-ex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using restricao_ex.Entities;
+using restricao_ex.Services;
+
+ List<Product> list = new List<Product>();
+
+            Console.Write("Número de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                string[] vect = Console.ReadLine().Split(',');
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vect[0], price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product p = calculationService.Max(list);
+
+            Console.WriteLine("Produto mais caro:");
+            Console.WriteLine(p);
