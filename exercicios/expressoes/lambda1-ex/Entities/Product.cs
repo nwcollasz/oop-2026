d01213594d0bdc69054f57420a8197efe81fa589
nwcollasz <nicollas.b@aluno.ifsp.edu.br>
2026-07-15 20:27:26 -0300

    exemplo de lambda

---
+++ exercicios/expressoes/lambda1-ex/Entities/Product.cs
@@ -0,0 +1,18 @@
+using System.Globalization;
+
+namespace lambda1_ex.Entities {
+    class Product {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ exercicios/expressoes/lambda1-ex/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using lambda1_ex.Entities;
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())); //lambda 
+
+            foreach (Product p in list) {
+                Console.WriteLine(p);
+            }
